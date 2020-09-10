ecec9aae2c811e08e4253e2a1eac628069586cf8
jonathanfruits <jonathan.t.fruits@gmail.com>
2020-09-10 01:23:57 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,22 @@
+using System;
+class MainClass {
+  public static void Main (string[] args) {
+    Console.WriteLine ("Enter the name of the item");
+    Console.ReadLine();
+    Console.WriteLine ("Enter the selling price of the item");
+    Console.ReadLine();
+    Console.WriteLine("Enter the sales tax");
+    Console.ReadLine();
+    Console.WriteLine("Enter the quantity of the item");
+   /* must find the final price of two items*/
+    Console.ReadLine();
+    Console.WriteLine("The cost is $1709.86");
+
+
+
+
+
+
+
+  }
+}
